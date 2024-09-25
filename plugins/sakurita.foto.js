ef61c84757d533fbe8b6a1e5a836f3815ddf2a12
ReeX7 <mendezvipz8@gmail.com>
2024-09-25 10:23:11 -0600

    Actualizar sakurita.foto.js

--- plugins/sakurita.foto.js
+++ plugins/sakurita.foto.js
@@ -13,7 +13,7 @@ let expp = Math.floor(Math.random() * 5000)
   global.db.data.users[m.sender].joincount += tok
   global.db.data.users[m.sender].exp += expp
 
-let texto = `*YerayBot-MD 💥*`
+let texto = `*BestBot-MD 💥*`
 
 const fkontak = {
         "key": {
